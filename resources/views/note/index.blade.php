20ad0d1befe3878a1415a99d8ffaa0c5909eb7a4
ThePagma <102959214+ThePagma@users.noreply.github.com>
2024-09-10 10:09:11 +0200

    created notes

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -5,7 +5,6 @@
         </a>
         <div class="notes">
             @foreach($notes as $note)
-
                 <div class="note">
                     <div class="note-body">
                         {{Str::words($note->note, 50)}}
@@ -24,7 +23,25 @@
                 </div>
                 <br>
             @endforeach
+                <div class="note">
+                    <div class="note-body">
+                        bas
+                    </div>
+                    <div class="note-buttons">
+                        <a href="http://crash-course-v4.test/note/106" class="note-edit-button">View</a>
+                        <a href="http://crash-course-v4.test/note/106/edit" class="note-edit-button">edit</a>
+
+                        <form action="http://crash-course-v4.test/note/106" method="post">
+                            <input type="hidden" name="_token" value="PdNrjrqo1w66wml8VzaVHONxu6UYtLinEYCUteLN" autocomplete="off">                            <input type="hidden" name="_method" value="DELETE">                            <button class="note-delete-button">Delete</button>
+
+                        </form>
+                    </div>
+                </div>
+                <br>
         </div>
+
+
+
 
         {{$notes->links()}}
     </div>
